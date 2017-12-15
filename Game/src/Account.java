67a6c1a4c28335971b7dd57acf5eb395e1e0d95f
Jens <hjerpe82@gmail.com>
2017-12-15 13:01:57 +0100

    Gjorde klart account

--- Game/src/Account.java
+++ Game/src/Account.java
@@ -3,8 +3,8 @@ import java.util.ArrayList;
 public class Account {
 
     private String password;
-    private String email;
-    private ArrayList<Character> listOfCharacters;
+    private final String email;
+    private ArrayList<PlayerCharacter> listOfCharacters;
     private ArrayList<Account> listOfFriends;
 
     public Account(String password, String email) {
@@ -14,23 +14,60 @@ public class Account {
         this.listOfFriends = new ArrayList<>();
     }
 
-    public boolean logIn(){
-        return true;
-    }
-    public boolean logOut(){
-        return true;
-    }
-    public boolean selectCharacter(){
-        return true;
-    }
-    public boolean deleteCharacter(){
-        return true;
-    }
-    //public Character createCharacter(){
-
-    //}
-    public boolean changePassword(){
-        return true;
+    public Character selectCharacter (String name){
+        for(PlayerCharacter playerCharacter : listOfCharacters){
+            if(playerCharacter.getName().equals(name)){
+                return playerCharacter;
+            }
+        }
+        throw new ArrayIndexOutOfBoundsException();
     }
 
+    public void deleteCharacter(String name){
+        for(PlayerCharacter playerCharacter : listOfCharacters){
+            if(playerCharacter.getName().equals(name)){
+                listOfCharacters.remove(playerCharacter);
+            }
+        }
+    }
+
+    public void createCharacter(PlayerCharacter newCharacter){
+        if(newCharacter instanceof Warrior){
+            Warrior newWarrior = (Warrior) newCharacter;
+            listOfCharacters.add(newWarrior);
+        }
+        else if(newCharacter instanceof Hunter){
+            Hunter newHunter = (Hunter) newCharacter;
+            listOfCharacters.add(newCharacter);
+        }
+        else if(newCharacter instanceof Mage){
+            Mage newMage = (Mage) newCharacter;
+        }
+    }
+
+    public void addFriend(Account friendAccount){
+        listOfFriends.add(friendAccount);
+
+    }
+
+    public void changePassword(String newPassword){
+
+        this.password = newPassword;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public ArrayList<PlayerCharacter> getListOfCharacters() {
+        return listOfCharacters;
+    }
+
+    public ArrayList<Account> getListOfFriends() {
+        return listOfFriends;
+    }
 }
